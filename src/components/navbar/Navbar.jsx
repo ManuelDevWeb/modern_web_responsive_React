02f6b89ec46abc09377c13c85bab90cddb9c2eb5
Manuel V <manueldeveloper17@gmail.com>
2022-01-04 01:01:05 -0500

    Feat: Se finalizó la construcción del navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 
 // Files
@@ -9,25 +9,60 @@ import './navbar.css';
 
 // BEM -> Block Element Modifier
 
+const Menu=()=>{
+    return(
+        <>
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wpgt3">What is GPT3?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+        </>
+    )
+}
+
 const Navbar = () => {
-    return ( 
+    // State para manejar el menu responsive
+    const [toggleMenu, setToggleMenu]=useState(false);
+
+    return (
+        // Navbar
         <div className='gpt3__navbar'>
             <div className='gpt3__navbar-links'>
                 <div className='gpt3__navbar-links_logo'>
                     <img src={logo} alt="logo" />
                 </div>
                 <div className='gpt3__navbar-links_container'>
-                    <p><a href="#home">Home</a></p>
-                    <p><a href="#wpgt3">What is GPT3?</a></p>
-                    <p><a href="#possibility">Open AI</a></p>
-                    <p><a href="#features">Case Studies</a></p>
-                    <p><a href="#blog">Library</a></p>
+                    <Menu />
                 </div>
             </div>
             <div className="gpt3__navbar-sign">
                 <p>Sign in</p>
                 <button type='button'>Sign up</button>
-            </div>        
+            </div>
+            {/* Navbar Responsive */}
+            <div className="gpt3__navbar-menu">
+                {
+                    // Validando el state de toggleMenu para mostar el icono
+                    toggleMenu 
+                        ? <RiCloseLine color='#fff' size={27} onClick={()=>setToggleMenu(false)} /> 
+                        : <RiMenu3Line color='#fff' size={27} onClick={()=>setToggleMenu(true)} />
+                }
+                {
+                    // Si el toggleMenu es verdadero, se mostrara el menu responsive
+                    toggleMenu && (
+                        <div className='gpt3__navbar-menu_container scale-up-center'>
+                            <div className="gpt3__navbar-menu_container-links">
+                                <Menu />
+                                <div className="gpt3__navbar-menu_container-links-sign">
+                                    <p>Sign in</p>
+                                    <button type='button'>Sign up</button>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
         </div>
     );
 }
