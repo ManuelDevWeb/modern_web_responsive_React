cf40c65f55c0013f91dd5dee916f620b81b25218
Manuel V <manueldeveloper17@gmail.com>
2022-01-10 20:28:04 -0500

    Se creo la seccion whatGPT3

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 
+// Data
+import {google, slack, atlassian, dropbox, shopify} from './imports';
+
 // Styles
 import './brand.css';
 
 const Brand = () => {
     return ( 
-        <div>
-            Brand
+        <div className='gpt3__brand section__padding'>
+            <div>
+                <img src={google} alt="google" />
+            </div>
+            <div>
+                <img src={slack} alt="slack" />
+            </div>
+            <div>
+                <img src={atlassian} alt="atlassian" />
+            </div>
+            <div>
+                <img src={dropbox} alt="dropbox" />
+            </div>
+            <div>
+                <img src={shopify} alt="shopify" />
+            </div>
         </div>
     );
 }
---
+++ src/components/brand/imports.js
@@ -0,0 +1,14 @@
+/* Archivo encargado de exportar logos */
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
+
+export {
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify
+}
